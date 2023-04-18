9979aecd841ad4c6a9753b6a7d1f513bfdef48c6
simenrau <simen.aune20@gmail.com>
2023-04-18 22:22:37 +0200

    Serve static files with express

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
-const express = require('express');
-const cors = require('cors');
-const axios = require('axios');
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/api/*', async (req, res) => {
-    try {
-        const apiResponse = await axios.get(`https://fantasy.premierleague.com${req.url}`);
-        res.json(apiResponse.data);
-    } catch (error) {
-        res.status(500).json({ error: 'An error occurred while fetching the data.' });
-    }
+app.get("/api/*", async (req, res) => {
+  try {
+    const apiResponse = await axios.get(
+      `https://fantasy.premierleague.com${req.url}`
+    );
+    res.json(apiResponse.data);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ error: "An error occurred while fetching the data." });
+  }
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
